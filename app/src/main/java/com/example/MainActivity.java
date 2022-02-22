3836e7c882f0fd2239edb6833724745e1026d501
mehedihasan03 <mdmehedi03@gmail.com>
2022-02-22 12:00:37 +0600

    add radio buttton and dropdown

--- app/src/main/java/com/example/MainActivity.java
+++ app/src/main/java/com/example/MainActivity.java
@@ -2,8 +2,12 @@ package com.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +19,9 @@ public class MainActivity extends AppCompatActivity {
     EditText email;
     EditText address;
     EditText password;
-
-
+    RadioButton selectedGen;
+    RadioGroup gender;
+    Spinner spinner;
 
 
     @Override
@@ -28,8 +33,15 @@ public class MainActivity extends AppCompatActivity {
         name = findViewById(R.id.tf_name);
         username = findViewById(R.id.tf_username);
         email = findViewById(R.id.tf_email);
+        gender = findViewById(R.id.radioGroup);
         address = findViewById(R.id.tf_address);
         password = findViewById(R.id.reg_pass);
+        spinner = findViewById(R.id.spinner);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
 
 
@@ -40,6 +52,9 @@ public class MainActivity extends AppCompatActivity {
                     System.out.println("Email : " + email.getText());
                     System.out.println("Address : " + address.getText());
                     System.out.println("Password : " + password.getText());
+                    int genId = gender.getCheckedRadioButtonId();
+                    selectedGen = findViewById(genId);
+                    System.out.println(selectedGen.getText());
                 }
         );
     }
